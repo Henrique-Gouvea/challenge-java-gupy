cf9f41e427b5a52da3e8b96c356aee97718edc84
Henrique-Gouvea <henrque.bonfim2@gmail.com>
2023-01-24 15:26:45 -0300

    feat: Criando classe funcionario

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,9 @@
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Funcionario func1 = new Funcionario("Henrique", LocalDate.of(1992, 04, 02), new BigDecimal("1500.50"), "Desenvolvedor");
+        System.out.println(func1.getNome());
     }
 }
